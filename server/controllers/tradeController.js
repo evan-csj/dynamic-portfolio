de4e5bce872f5177fea2e5ec9d678a7e9f046a90
Evan Cheng <evancheng2014@gmail.com>
2023-04-24 01:49:06 -0700

    update trade control with async

--- server/controllers/tradeController.js
+++ server/controllers/tradeController.js
@@ -1,6 +1,5 @@
 const { v4: uuid } = require('uuid');
 const knex = require('knex')(require('../knexfile'));
-const validation = require('./validation');
 const update = require('./update');
 
 const tradeHistory = (req, res) => {
@@ -25,161 +24,225 @@ const tradeHistory = (req, res) => {
         });
 };
 
-const addTrade = (req, res) => {
-    let enoughCash = false;
-    let enoughShares = false;
-    const tradeDetail = req.body;
+const addTrade = async (req, res) => {
+    const { user_id: userId, type, currency, price, shares, ticker } = req.body;
+    const validType = ['buy', 'sell'];
+    const validCurrency = ['usd', 'cad'];
 
-    if (!validation.tradeTypeValidate(tradeDetail.type)) {
-        return res.status(400).send('Invalid trade type');
+    if (!validType.includes(type)) {
+        return res.status(422).json({ error: 'Invalid funding type' });
     }
 
-    const newTrade = { id: uuid(), ...tradeDetail };
+    if (!validCurrency.includes(currency)) {
+        return res.status(422).json({ error: 'Invalid fund currency' });
+    }
 
-    // check if enough cash for buy
-    if (newTrade.type === 'buy') {
-        knex('user')
+    if (isNaN(price) || price <= 0) {
+        return res.status(422).json({ error: 'Invalid price' });
+    }
+
+    if (isNaN(shares) || shares <= 0) {
+        return res.status(422).json({ error: 'Invalid shares' });
+    }
+
+    console.log('userid', userId);
+    console.log('ticket', ticker);
+
+    try {
+        const userData = await knex('user')
             .select('cash_cad', 'cash_usd')
-            .where('id', newTrade.user_id)
-            .then(data => {
-                if (data.length === 0) {
-                    return res
-                        .status(404)
-                        .json(
-                            `The user with id ${newTrade.user_id} is not found!`
-                        );
-                } else {
-                    const amountRequired = newTrade.price * newTrade.shares;
-                    let cashUSD = data[0].cash_usd;
-                    let cashCAD = data[0].cash_cad;
+            .where({ id: userId })
+            .first();
 
-                    if (
-                        newTrade.currency === 'usd' &&
-                        cashUSD >= amountRequired
-                    ) {
-                        enoughCash = true;
-                        cashUSD -= amountRequired;
-                    } else if (
-                        newTrade.currency === 'cad' &&
-                        cashCAD >= amountRequired
-                    ) {
-                        enoughCash = true;
-                        cashCAD -= amountRequired;
-                    } else {
-                        if (newTrade.currency === 'usd')
-                            return res.json(
-                                `${cashUSD} USD is not enough for buy ${amountRequired}`
-                            );
 
-                        if (newTrade.currency === 'cad')
-                            return res.json(
-                                `${cashCAD} CAD is not enough for buy ${amountRequired}`
-                            );
-                    }
+        const holdingData = await knex('holding')
+            .select('buy_shares', 'sell_shares', 'avg_price')
+            .where({ user_id: userId })
+            .andWhere({ ticker: ticker })
+            .first();
 
-                    knex('user')
-                        .update({
-                            cash_cad: cashCAD,
-                            cash_usd: cashUSD,
-                        })
-                        .where('user_id', newTrade.user_id)
-                        .then(data => {
-                            console.log(data);
-                        });
-                }
-            });
+        if (!userData || !holdingData)
+            return res.status(404).json({ error: 'User not found' });
+
+        let { cash_cad: cashCAD, cash_usd: cashUSD } = userData;
+        let {
+            buy_shares: buyShares,
+            sell_shares: sellShares,
+            avg_price: avgPrice,
+        } = holdingData;
+        const amountRequired = price * shares;
+
+        // check your cash for buy
+        if (type === 'buy') {
+            if (currency === 'usd' && cashUSD >= amountRequired) {
+                cashUSD -= amountRequired;
+            } else if (currency === 'cad' && cashCAD >= amountRequired) {
+                cashCAD -= amountRequired;
+            } else {
+                return res.status(422).json({ error: 'Insufficient cash' });
+            }
+        }
+
+        // check your shares for sell
+        if (type === 'sell') {
+            const sharesHold = buyShares - sellShares;
+            if (sharesHold => shares) {
+                sellShares += shares;
+            } else {
+                return res.status(422).json({ error: 'Insufficient shares' });
+            }
+        }
+        return res.status(200).json('good')
+    } catch (error) {
+        return res.status(500).json({ error: 'Something went wrong' });
     }
 
-    // check if enough share for sell
-    if (newTrade.type === 'sell') {
-        knex('holding')
-            .select('buy_shares', 'sell_shares')
-            .where('ticker', newTrade.ticker)
-            .andWhere('user_id', newTrade.user_id)
-            .then(data => {
-                if (data.length === 0) {
-                    return res
-                        .status(404)
-                        .json(`The stock with ticker ${ticker} is not found!`);
-                } else {
-                    console.log(data);
-                    const sharesHold = data[0].buy_shares - data[0].sell_shares;
-                    if (sharesHold >= newTrade.shares) {
-                        enoughShares = true;
-                    } else {
-                        return res.json(
-                            `${sharesHold} shares in holding is not enough for sell ${newTrade.shares} shares`
-                        );
-                    }
-                }
-            });
-    }
+    // const newTrade = { id: uuid(), ...tradeDetail };
 
-    newTrade.order_status = 'approve';
+    // // check if enough cash for buy
+    // if (newTrade.type === 'buy') {
+    //     knex('user')
+    //         .select('cash_cad', 'cash_usd')
+    //         .where('id', newTrade.user_id)
+    //         .then(data => {
+    //             if (data.length === 0) {
+    //                 return res
+    //                     .status(404)
+    //                     .json(
+    //                         `The user with id ${newTrade.user_id} is not found!`
+    //                     );
+    //             } else {
+    //                 const amountRequired = newTrade.price * newTrade.shares;
+    //                 let cashUSD = data[0].cash_usd;
+    //                 let cashCAD = data[0].cash_cad;
 
-    if (enoughCash || enoughShares) {
-        knex('trade')
-            .insert(newTrade)
-            .then(data => {
-                console.log(data);
-            });
+    //                 if (
+    //                     newTrade.currency === 'usd' &&
+    //                     cashUSD >= amountRequired
+    //                 ) {
+    //                     enoughCash = true;
+    //                     cashUSD -= amountRequired;
+    //                 } else if (
+    //                     newTrade.currency === 'cad' &&
+    //                     cashCAD >= amountRequired
+    //                 ) {
+    //                     enoughCash = true;
+    //                     cashCAD -= amountRequired;
+    //                 } else {
+    //                     if (newTrade.currency === 'usd')
+    //                         return res.json(
+    //                             `${cashUSD} USD is not enough for buy ${amountRequired}`
+    //                         );
 
-        console.log('debug');
-        knex('holding')
-            .select('avg_price', 'buy_shares', 'sell_shares')
-            .where('ticker', newTrade.ticker)
-            .andWhere('user_id', newTrade.user_id)
-            .then(data => {
-                if (data.length === 1) {
-                    let newAvg = data[0].avg_price;
-                    let buyShares = data[0].buy_shares;
-                    let sellShares = data[0].sell_shares;
+    //                     if (newTrade.currency === 'cad')
+    //                         return res.json(
+    //                             `${cashCAD} CAD is not enough for buy ${amountRequired}`
+    //                         );
+    //                 }
 
-                    if (newTrade.type === 'buy') {
-                        const currentHoldShares =
-                            data[0].buy_shares - data[0].sell_shares;
-                        newAvg = update.averagePrice(
-                            newTrade.price,
-                            newTrade.shares,
-                            data[0].avg_price,
-                            currentHoldShares
-                        );
-                        buyShares += newTrade.shares;
-                    } else {
-                        sellShares += newTrade.shares;
-                    }
+    //                 knex('user')
+    //                     .update({
+    //                         cash_cad: cashCAD,
+    //                         cash_usd: cashUSD,
+    //                     })
+    //                     .where('user_id', newTrade.user_id)
+    //                     .then(data => {
+    //                         console.log(data);
+    //                     });
+    //             }
+    //         });
+    // }
 
-                    knex('holding')
-                        .update({
-                            avg_price: newAvg,
-                            buy_shares: buyShares,
-                            sell_shares: sellShares,
-                        })
-                        .where('ticker', newTrade.ticker)
-                        .andWhere('user_id', newTrade.user_id)
-                        .then(data => {
-                            console.log(data);
-                        });
-                } else if (data.length === 0) {
-                    const newHolding = {
-                        id: uuid(),
-                        user_id: newTrade.user_id,
-                        ticker: newTrade.ticker,
-                        avg_price: newTrade.price,
-                        buy_shares: newTrade.shares,
-                        sell_shares: 0,
-                        currency: newTrade.currency,
-                    };
-                    knex('holding')
-                        .insert(newHolding)
-                        .then(data => {
-                            console.log(data);
-                        });
-                } else {
-                    return res.status(400).json('Fail to find the holding');
-                }
-            });
-    }
+    // // check if enough share for sell
+    // if (newTrade.type === 'sell') {
+    //     knex('holding')
+    //         .select('buy_shares', 'sell_shares')
+    //         .where('ticker', newTrade.ticker)
+    //         .andWhere('user_id', newTrade.user_id)
+    //         .then(data => {
+    //             if (data.length === 0) {
+    //                 return res
+    //                     .status(404)
+    //                     .json(`The stock with ticker ${ticker} is not found!`);
+    //             } else {
+    //                 console.log(data);
+    //                 const sharesHold = data[0].buy_shares - data[0].sell_shares;
+    //                 if (sharesHold >= newTrade.shares) {
+    //                     enoughShares = true;
+    //                 } else {
+    //                     return res.json(
+    //                         `${sharesHold} shares in holding is not enough for sell ${newTrade.shares} shares`
+    //                     );
+    //                 }
+    //             }
+    //         });
+    // }
+
+    // newTrade.order_status = 'approve';
+
+    // if (enoughCash || enoughShares) {
+    //     knex('trade')
+    //         .insert(newTrade)
+    //         .then(data => {
+    //             console.log(data);
+    //         });
+
+    //     console.log('debug');
+    //     knex('holding')
+    //         .select('avg_price', 'buy_shares', 'sell_shares')
+    //         .where('ticker', newTrade.ticker)
+    //         .andWhere('user_id', newTrade.user_id)
+    //         .then(data => {
+    //             if (data.length === 1) {
+    //                 let newAvg = data[0].avg_price;
+    //                 let buyShares = data[0].buy_shares;
+    //                 let sellShares = data[0].sell_shares;
+
+    //                 if (newTrade.type === 'buy') {
+    //                     const currentHoldShares =
+    //                         data[0].buy_shares - data[0].sell_shares;
+    //                     newAvg = update.averagePrice(
+    //                         newTrade.price,
+    //                         newTrade.shares,
+    //                         data[0].avg_price,
+    //                         currentHoldShares
+    //                     );
+    //                     buyShares += newTrade.shares;
+    //                 } else {
+    //                     sellShares += newTrade.shares;
+    //                 }
+
+    //                 knex('holding')
+    //                     .update({
+    //                         avg_price: newAvg,
+    //                         buy_shares: buyShares,
+    //                         sell_shares: sellShares,
+    //                     })
+    //                     .where('ticker', newTrade.ticker)
+    //                     .andWhere('user_id', newTrade.user_id)
+    //                     .then(data => {
+    //                         console.log(data);
+    //                     });
+    //             } else if (data.length === 0) {
+    //                 const newHolding = {
+    //                     id: uuid(),
+    //                     user_id: newTrade.user_id,
+    //                     ticker: newTrade.ticker,
+    //                     avg_price: newTrade.price,
+    //                     buy_shares: newTrade.shares,
+    //                     sell_shares: 0,
+    //                     currency: newTrade.currency,
+    //                 };
+    //                 knex('holding')
+    //                     .insert(newHolding)
+    //                     .then(data => {
+    //                         console.log(data);
+    //                     });
+    //             } else {
+    //                 return res.status(400).json('Fail to find the holding');
+    //             }
+    //         });
+    // }
 };
 
 module.exports = { tradeHistory, addTrade };
