5cd15d1e7177df6bf91d5690be2238ebc9e26167
Evan Cheng <evancheng2014@gmail.com>
2023-04-30 21:25:47 -0700

    add fonts

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './styles/theme';
+import Fonts from './styles/fonts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <ChakraProvider theme={theme}>
+            <Fonts />
+            <App />
+        </ChakraProvider>
     </React.StrictMode>
 );
 
---
+++ client/src/styles/theme.js
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    colors: {
+        light: {
+            navy: '#10316B',
+            blue: '#0B409C',
+            yellow: '#FFCE63',
+            white: '#F2F7FF',
+        },
+    },
+});
+
+export default theme;
