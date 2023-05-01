356446943a99e1e6bd38ac853b521788c1b1889f
Evan Cheng <evancheng2014@gmail.com>
2023-04-30 19:49:51 -0700

    add jost font

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,34 @@
 // import '@components/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme/theme';
+import localFont from 'next/font/local'
+
+const jost = localFont({
+    src: [
+      {
+        path: '../assets/fonts/Jost-Bold.ttf',
+        weight: '700',
+        style: 'normal',
+      },
+      {
+        path: '../assets/fonts/Jost-Regular.ttf',
+        weight: '400',
+        style: 'normal',
+      },
+      {
+        path: '../assets/fonts/Jost-Light.ttf',
+        weight: '300',
+        style: 'normal',
+      },
+    ],
+  })
 
 export default function App({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            <main className={jost.className}>
+                <Component {...pageProps} />
+            </main>
         </ChakraProvider>
     );
 }
