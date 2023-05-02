44efb7af19ca37d6e5cd0de5b3cc70603fdc3c75
Evan Cheng <evancheng2014@gmail.com>
2023-05-02 01:39:50 -0700

    add history components

--- client/src/components/History.js
+++ client/src/components/History.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Heading, Flex, Box, Center } from '@chakra-ui/react';
+import '../styles/global.scss';
 
 function History() {
-    return <div>History</div>;
+    return (
+        <Flex className="flex-col">
+            <Center bg="light.navy" color="light.white" h={12} borderBottomColor='light.yellow' borderBottomWidth={4}>
+                <Heading size='md'>Transaction History</Heading>
+            </Center>
+        </Flex>
+    );
 }
 
 export default History;
