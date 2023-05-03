69d17fec8390ea6c361c21851a97dc7c0f38f9b3
Evan Cheng <evancheng2014@gmail.com>
2023-05-03 02:41:06 -0700

    add popover

--- client/src/components/MainTab.js
+++ client/src/components/MainTab.js
@@ -1,9 +1,58 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Flex, Box } from '@chakra-ui/react';
+import {
+    Flex,
+    Box,
+    Button,
+    ButtonGroup,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverAnchor,
+    useDisclosure,
+    FocusLock,
+} from '@chakra-ui/react';
 import { Profile, CandleStick, History, Login, Fund } from '../styles/icons';
+import { color } from 'framer-motion';
+
+const TextInput = React.forwardRef((props, ref) => {
+    return (
+        <FormControl>
+            <FormLabel htmlFor={props.id}>{props.label}</FormLabel>
+            <Input ref={ref} id={props.id} {...props} />
+        </FormControl>
+    );
+});
+
+const Form = ({ amountRef }) => {
+    return (
+        <Stack spacing={8}>
+            <TextInput
+                label="Amount"
+                id="amount"
+                ref={amountRef}
+                defaultValue="0"
+            />
+            <ButtonGroup display="flex" justifyContent="space-between">
+                <Button color="light.red">Withdraw</Button>
+                <Button color="light.green">Deposit</Button>
+            </ButtonGroup>
+        </Stack>
+    );
+};
 
 function MainTab() {
+    const { onOpen, onClose, isOpen } = useDisclosure();
+    const amountRef = React.useRef(null);
+
     return (
         <>
             <Box h={20}></Box>
@@ -25,7 +74,26 @@ function MainTab() {
                 <NavLink to="/candlestick">
                     <CandleStick variant="btn" />
                 </NavLink>
-                <Fund variant="btn" />
+
+                <Popover
+                    isOpen={isOpen}
+                    initialFocusRef={amountRef}
+                    onOpen={onOpen}
+                    onClose={onClose}
+                    placement="top"
+                    closeOnBlur={false}
+                >
+                    <PopoverTrigger>
+                        <Fund variant="btn" />
+                    </PopoverTrigger>
+                    <PopoverContent p={4} mb={2}>
+                        <FocusLock returnFocus persistentFocus={false}>
+                            <PopoverArrow />
+                            <Form amountRef={amountRef} />
+                        </FocusLock>
+                    </PopoverContent>
+                </Popover>
+
                 <NavLink to="/history">
                     <History variant="btn" />
                 </NavLink>
