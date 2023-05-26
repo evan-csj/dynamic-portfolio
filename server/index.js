479fc0594ae2e8ce6c63ae4ed9366aa127aa7a7e
Evan Cheng <evancheng2014@gmail.com>
2023-05-25 18:50:11 -0700

    remove websocket from backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const WebSocket = require('ws');
 
 const userRoute = require('./routes/userRoute');
 const tradeRoute = require('./routes/tradeRoute');
@@ -12,10 +11,6 @@ const watchlistRoute = require('./routes/watchlistRoute');
 const portfolioRoute = require('./routes/portfolioRoute');
 const symbolRoute = require('./routes/symbolRoute');
 const websocketRoute = require('./routes/websocketRoute');
-
-// const ws = new WebSocket(
-//     'wss://ws.finnhub.io?token=chcq92hr01qm1ei3tmi0chcq92hr01qm1ei3tmig'
-// );
 
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
@@ -32,16 +27,6 @@ app.use('/watchlist', watchlistRoute);
 app.use('/portfolio', portfolioRoute);
 app.use('/symbols', symbolRoute);
 app.use('/websocket', websocketRoute);
-
-// ws.on('error', console.error);
-
-// ws.on('open', function open() {
-//     ws.send(JSON.stringify({ type: 'subscribe', symbol: 'BINANCE:BTCUSDT' }));
-// });
-
-// ws.on('message', function message(data) {
-//     console.log('received: %s', data);
-// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
